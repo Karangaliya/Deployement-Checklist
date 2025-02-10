f19ad68eb7a371b35d98f555efac262952cc8692
Karangaliya <galiyak611@gmail.com>
2025-02-10 10:52:52 +0530

    Adding priority

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -1,17 +1,6 @@
 "use client";
-import { useState,useEffect } from "react";
-import { useMsal } from "@azure/msal-react";
-import { useRouter } from "next/navigation";
-import { MsalProvider } from "@azure/msal-react";
-import { PublicClientApplication } from "@azure/msal-browser";
-import { msalConfig } from "../config/msalConfig";
+import { redirect } from "next/navigation";
 
 export default function Home() {
-const router = useRouter();
-
-  useEffect(() => {
-      router.push("/login")
-  }, [])
-
-  return ""
+  redirect("/login")
 }
